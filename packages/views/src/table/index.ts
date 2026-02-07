a75c79babaa9e16aa52330eadd756d7a253ec2aa
Chris Smothers <chris.smothers@gmail.com>
2026-02-07 12:21:06 +0800

    feat(views): add VirtualizedTableView with X+Y dual-axis virtualization
    - Add VirtualizedTableView component with row and column virtualization
    - Add performance optimizations (RowCache, CellRendererCache, useBatchedRows, etc.)
    - Add 27 tests for virtualized table and optimization utilities
    - Update validation gates in plan docs (Phase 2 complete)

--- packages/views/src/table/index.ts
+++ packages/views/src/table/index.ts
@@ -3,6 +3,7 @@
  */
 
 export { TableView, type TableViewProps, type ColumnUpdate } from './TableView.js'
+export { VirtualizedTableView, type VirtualizedTableViewProps } from './VirtualizedTableView.js'
 export { TableHeader, type TableHeaderProps } from './TableHeader.js'
 export { TableCell, type TableCellProps } from './TableCell.js'
 export {
@@ -11,3 +12,12 @@ export {
   type UseTableStateOptions,
   type UseTableStateResult
 } from './useTableState.js'
+export {
+  useBatchedRows,
+  useScrollDebounce,
+  useIntersectionObserver,
+  useStableCallback,
+  useThrottle,
+  RowCache,
+  CellRendererCache
+} from './optimizations.js'
